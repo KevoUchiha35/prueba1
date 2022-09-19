c07069e702300f20ac139b9229008f7633102861
KevoUchiha35 <sergiokevin14@gmail.com>
2022-09-19 11:00:39 -0500

    static

--- primerproyecto/apptesca/views.py
+++ primerproyecto/apptesca/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 def kevo(request):
-    return render (request, 'apptesca/prueba.html')
+    return render (request, 'prueba.html')
     
 # Create your views here.
  
